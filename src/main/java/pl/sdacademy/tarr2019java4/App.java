0a310b8ed722bde8c847ea74de88cb73a085dae9
Jakub Olszewski <j.b.olszewski@gmail.com>
2019-11-23 18:03:58 +0100

    init

---
+++ src/main/java/pl/sdacademy/tarr2019java4/App.java
@@ -0,0 +1,26 @@
+package pl.sdacademy.tarr2019java4;
+//generate test
+//alt + enter
+
+/**
+ * App
+ *
+ * @author: Jakub Olszewski [http://github.com/jakub-olszewski]
+ * @date: 23.11.2019 18:00
+ **/
+public class App {
+    //code format
+    //ctrl+alt+L
+
+    // alt + insert + enter
+    // generate code
+
+    //psvm + tab
+    public static void main(String[] args) {
+        //sout + tab
+        System.out.println("Hello World!");
+
+      
+    }
+
+}
