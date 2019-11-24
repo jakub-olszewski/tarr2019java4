452778dc3f881077cad666225c74a09326b1a3a3
Jakub Olszewski <j.b.olszewski@gmail.com>
2019-11-24 11:47:45 +0100

    Teoria tworzenie metody

--- src/main/java/pl/sdacademy/tarr2019java4/App.java
+++ src/main/java/pl/sdacademy/tarr2019java4/App.java
@@ -31,6 +31,21 @@ public class App {
         //skrót : sout
         System.out.println(rok);
         System.out.println("rok");// to jest napis w cudzysłowiu
+
+        int c; // deklaracja zmiennej c, która zawiera sumę
+        c = suma(6,89); // przypisanie wyniku metody do zmiennej c
+        System.out.println(c);
     }
+
+    /**
+     * Metoda sumująca liczby a i b
+     * @param a liczba całkowita
+     * @param b liczba całkowita
+     * @return wynik sumy a+b
+     */
+    private static int suma(int a, int b) {
+        return a+b;
+    }
+
 
 }
