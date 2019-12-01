6d718f2f83e35d608965d4eb869aa58aaf8b6808
Jakub Olszewski <j.b.olszewski@gmail.com>
2019-12-01 15:02:27 +0100

    Zadania domowe do realizacji w pakiecie zadania

---
+++ src/main/java/pl/sdacademy/tarr2019java4/javapodstawy/zadania/ZadanieL1.java
@@ -0,0 +1,10 @@
+package pl.sdacademy.tarr2019java4.javapodstawy.zadania;
+
+public class ZadanieL1 {
+
+    public static void main(String[] args) {
+
+        // rozwiazanie zadania
+        //for ()
+    }
+}
