d4b40df666a2107e7a766f1dd6a24d249b895657
Jakub Olszewski <j.b.olszewski@gmail.com>
2019-12-01 14:37:43 +0100

    Zadanie: Pętla while

---
+++ src/main/java/pl/sdacademy/tarr2019java4/javapodstawy/petle/petlawhile/WhileMain.java
@@ -0,0 +1,22 @@
+package pl.sdacademy.tarr2019java4.javapodstawy.petle.petlawhile;
+
+public class WhileMain {
+
+    public static void main(String[] args) {
+
+        int liczba = 0;
+        while (liczba<10){
+            System.out.print(liczba+",");
+            // Zadanie
+            // zrób tak aby na końcu nie było przecinka
+            liczba++;
+        }
+        
+        // Zadanie: wypisz w pętli
+        // while liczby podzielne przez 3 od
+        // 0 do 100
+        // Dodatkowo : zlicz ile ich jest
+
+
+    }
+}
