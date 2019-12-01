32d505f7740b1c81461300a309ea1ac4fe2a51db
Jakub Olszewski <j.b.olszewski@gmail.com>
2019-12-01 09:47:24 +0100

    Rozwiązanie: Stwórz enum Status i wykonaj implementacje metod Sprawdź, przetestuj wyniki metod

--- src/main/java/pl/sdacademy/tarr2019java4/javapodstawy/instrukcje/warunkowe/Sprawdz.java
+++ src/main/java/pl/sdacademy/tarr2019java4/javapodstawy/instrukcje/warunkowe/Sprawdz.java
@@ -5,25 +5,26 @@ public class Sprawdz {
     // metoda sprawdzajaca pelnoletniosc
     // boolean jestPelnoletni(int wiek)
 
-    public static boolean czyJestPelnoletni(int wiek){
-        return false;
+    public static boolean czyJestPelnoletni(int wiek) {
+        return wiek>17;
     }
 
-    public static boolean czyJestAktywny(Status status){
-
+    public static boolean czyJestAktywny(Status status) {
+        return status.equals(Status.AKTYWNY);
     }
 
     // string toLowerCase()
-    public static boolean czyJestKobieta(String plec){
+    public static boolean czyJestKobieta(String plec) {
         return plec.toLowerCase().equals("kobieta");
     }
 
     /**
      * Wersja powyższej metody z użyciem typu wyliczeniowego
+     *
      * @param plecOsoby
      * @return prawda/fałsz
      */
-    public static boolean czyJestKobieta(Plec plecOsoby){
+    public static boolean czyJestKobieta(Plec plecOsoby) {
         return plecOsoby.equals(Plec.KOBIETA);
     }
 }
---
+++ src/main/java/pl/sdacademy/tarr2019java4/javapodstawy/instrukcje/warunkowe/Status.java
@@ -0,0 +1,6 @@
+package pl.sdacademy.tarr2019java4.javapodstawy.instrukcje.warunkowe;
+
+public enum Status {
+
+    ZALOGOWANY,NIE_ZALOGOWANY,AKTYWNY,NIE_AKTYWNY
+}
