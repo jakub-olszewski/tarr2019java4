f8eb26406ff0be88e29497a28d69515ca4150047
Jakub Olszewski <j.b.olszewski@gmail.com>
2019-12-01 09:49:57 +0100

    Zadanie: Data i czas

---
+++ src/main/java/pl/sdacademy/tarr2019java4/javapodstawy/dataiczas/DataICzasMain.java
@@ -0,0 +1,20 @@
+package pl.sdacademy.tarr2019java4.javapodstawy.dataiczas;
+
+import java.time.LocalDateTime;
+
+public class DataICzasMain {
+
+    public static void main(String[] args) {
+
+        LocalDateTime teraz = LocalDateTime.now();
+        System.out.println("Teraz jest "+teraz);
+
+        String formatDaty = "yyyy-MM-dd HH:mm:ss";
+        String czasPosilku = "2019-12-01 13:00:00";
+        // Zadanie
+        // zamienić czas posilku na date wedlug formatu
+
+        // Zadanie
+        // wyswietlic date obecną według formatu
+    }
+}
