bae46747531bc05ed769dcf0cc94bcf17a0976fb
Jakub Olszewski <j.b.olszewski@gmail.com>
2019-12-14 13:54:10 +0100

    Odpowiedź: Skaner i wyrażenie regularne

--- src/main/java/pl/sdacademy/tarr2019java4/javapodstawy/wyrazenia/WyrazeniaMain.java
+++ src/main/java/pl/sdacademy/tarr2019java4/javapodstawy/wyrazenia/WyrazeniaMain.java
@@ -7,27 +7,27 @@ import java.util.regex.Pattern;
 public class WyrazeniaMain {
 
     public static void main(String[] args) {
+        {
+            // wzorzec wyszukiwania
+            String wyrazenieRegularne = "\\d{3}";//"Marcin";
+            //Pattern compiledPattern = Pattern.compile("\\d{3}");
+            Pattern compiledPattern = Pattern.compile(wyrazenieRegularne);
 
-        // wzorzec wyszukiwania
-        String wyrazenieRegularne = "\\d{3}";//"Marcin";
-        //Pattern compiledPattern = Pattern.compile("\\d{3}");
-        Pattern compiledPattern = Pattern.compile(wyrazenieRegularne);
+            // zamiast \\d{3} można wpisać Marcin
 
-        // zamiast \\d{3} można wpisać Marcin
+            // porównywarkę matchera
+            Matcher matcher = compiledPattern.matcher("A tata Marcina powiedział 007");
 
-        // porównywarkę matchera
-        Matcher matcher = compiledPattern.matcher("A tata Marcina powiedział 007");
+            System.out.println("Czy występuje częściowo ? " + matcher.find());
+            // czy jest w danym tekscie jako fragment
 
-        System.out.println("Czy występuje częściowo ? "+matcher.find());
-        // czy jest w danym tekscie jako fragment
+            System.out.println("Czy jest dokładnie taki jaki szukamy ? " + matcher.matches());
+            // czy jest dokładnie taki sam - pasujący do wzorca
 
-        System.out.println("Czy jest dokładnie taki jaki szukamy ? "+matcher.matches());
-        // czy jest dokładnie taki sam - pasujący do wzorca
-
-        String tekst = "123-123-123";
-        matcher = compiledPattern.matcher(tekst);
-        System.out.println("Czy występuje wzorzec '"+wyrazenieRegularne+"' w tekscie '"+tekst+"' ? "+matcher.matches());
-
+            String tekst = "123-123-123";
+            matcher = compiledPattern.matcher(tekst);
+            System.out.println("Czy występuje wzorzec '" + wyrazenieRegularne + "' w tekscie '" + tekst + "' ? " + matcher.matches());
+        }
         /**
          * Zadanie: Stwórz program który pobiera z klawiatury numer telefonu
          * gdy jest poprawny - "Numer został poprawnie wprowadzony"
@@ -40,27 +40,41 @@ public class WyrazeniaMain {
          *
          * Dodatkowo: numer może mieć ale nie musi +48
          */
-        System.out.println("\n====== TEST WPROWADZANIA NUMERU TELEFONU ======");
-        // utworzenie skanera
-        Scanner skaner = new Scanner(System.in);
+        {
+            System.out.println("\n====== TEST WPROWADZANIA NUMERU TELEFONU ======");
+            // utworzenie skanera
+            Scanner skaner = new Scanner(System.in);
 
-        // pytanie o wprowadzenie numeru
-        System.out.println("Wprowadź numer telefonu: ");
+            // for while ?
+            while (true){// pętla w nieskończoność
+                // pytanie o wprowadzenie numeru
+                System.out.println("Wprowadź numer telefonu: ");
 
-        // pobieranie z klawiatury do zmiennej
-        String odpowiedz = skaner.nextLine();
+                // pobieranie z klawiatury do zmiennej
+                String odpowiedz = skaner.nextLine();
 
-        // porównanie ze wzorcem
+                // porównanie ze wzorcem
 
-        // wynik porównania - informacja na konsole
-        boolean czyPasuje = false;
+                // wynik porównania - informacja na konsole
+                String wyrazenieRegularne = "\\d{3}-\\d{3}-\\d{3}";
 
-        // gdy wynik porównania jest ok przerwanie pętli
-        if(czyPasuje){
-            // przerwanie pętli
+                Pattern compiledPattern = Pattern.compile(wyrazenieRegularne);
+                Matcher matcher = compiledPattern.matcher(odpowiedz);
+
+                boolean czyPasuje = matcher.matches();// .matches()
+
+                // gdy wynik porównania jest ok przerwanie pętli
+                if (czyPasuje) {
+                    System.out.println("Numer poprawny :)");
+                    break;// zakonczenie pętli
+                    // przerwanie pętli
+                }else{
+                    System.out.println("Numer nie poprawny!");
+                }
+            }
+
+
+
         }
-
-
-
     }
 }
