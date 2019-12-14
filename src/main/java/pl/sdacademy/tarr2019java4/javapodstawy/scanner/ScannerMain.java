6dbd7d66a6a3423c658008270afe8dd6d7fd55d0
Jakub Olszewski <j.b.olszewski@gmail.com>
2019-12-14 11:58:17 +0100

    Zadanie: Dokończyć wprowadzanie liczb

---
+++ src/main/java/pl/sdacademy/tarr2019java4/javapodstawy/scanner/ScannerMain.java
@@ -0,0 +1,57 @@
+package pl.sdacademy.tarr2019java4.javapodstawy.scanner;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class ScannerMain {
+
+    public static void main(String[] args) {
+        /*
+        Utwórz tablice zawierającą liczby wczytane za pomocą scanner
+        użyj nextInt()
+
+        warunki zakończenia:
+        * na początku pytanie ile liczb chcesz wprowadzić
+        * po każdym dodaniu pytanie czy chcesz zakończyć [t/n]
+
+
+        Dla chętnych:
+        Utwórz tablice zawierającą liczby wylosowane z użyciem Random()
+         */
+
+
+        Scanner skaner = new Scanner(System.in);
+        System.out.println("Jak masz na imię ?");
+        String wprowadzoneImie = skaner.nextLine();// wczytanie napisu
+        System.out.println("\nCześć, "+wprowadzoneImie+" miło Cię poznać");
+        System.out.println("Wprowadź liczby. Ile masz dla mnie liczb ? ");
+        int iloscLiczb = skaner.nextInt();
+        Integer[] tablica = new Integer[iloscLiczb];
+        // a) pętla i warunek zakończenia (for)
+        for(int i=0; i < iloscLiczb;i++){
+            tablica[i] = skaner.nextInt();
+        }
+        System.out.println("Wprowadziłeś:");
+        for (int liczba : tablica){
+            System.out.print(liczba+" ");
+        }
+        // b) pętla (while) i ilosc--
+
+        //================ wersja z pytaniem
+        // w pętli
+        boolean czyKoniecPobierania = false;
+        List<Integer> listaLiczb = new ArrayList<>();
+        while (!czyKoniecPobierania){
+            System.out.println("Wprowadź liczbę:");
+            listaLiczb.add(skaner.nextInt());
+            System.out.println("Czy chcesz zakończyć wprowadzanie [t/n] ?");
+            if(/*tak*/){
+                // ma się zakończyć
+            }
+        }
+        // wypisanie wprowadzonych liczb
+
+
+    }
+}
